a9bd2f3e912299e759f3d22f9eabfdc3f1a38482
suryadeomodi <suryadeomodi@gmail.com>
2022-07-10 15:48:51 +0530

    changed m2 to m4

--- src/main/java/in/ashokit/bindings/Test.java
+++ src/main/java/in/ashokit/bindings/Test.java
@@ -1,22 +1,21 @@
 package in.ashokit.bindings;
 
 public class Test {
- public static int m1(){
-	  return 0;
-	  }
-	
-  
-  public static int m2(){
-	  return 0;
-	  }
-  public static void main(String[] args) {
-	  
-	  String s1="hello bhaiya should be removed";
-	  
-	  
-	  String s2="sjdhs";
-	  
-	System.out.println(s1+s2);
-}
- 
+	public static int m1() {
+		return 0;
+	}
+
+	public static int m4() {
+		return 0;
+	}
+
+	public static void main(String[] args) {
+
+		String s1 = "hello bhaiya should be removed";
+
+		String s2 = "sjdhs";
+
+		System.out.println(s1 + s2);
+	}
+
 }
